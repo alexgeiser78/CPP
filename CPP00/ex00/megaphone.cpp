6033cd7464ef483b8155af48690737d8cc5a871b
Pleiades <113288425+alexgeiser78@users.noreply.github.com>
2024-02-24 20:00:50 +0100

    24.02.24

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ageiser <ageiser@student.42barcelo>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 19:12:57 by ageiser           #+#    #+#             */
+/*   Updated: 2024/02/24 19:13:00 by ageiser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int i;
+    int j;
+
+    
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+    else if (argc > 1)
+    {
+        i = 1;
+        while (argv[i])
+        {
+            j = 0;
+            while (argv[i][j])
+            {
+                std::cout << (char)toupper(argv[i][j]);
+                j++;
+            }
+            i++;
+        }
+        std::cout << std::endl;
+    }
+return (0);
+}
