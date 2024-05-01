de710e323191e39cef1bd6c5d4f0ed9c710054db
Alexandre Geiser <ageiser@car11s6.42barcelona.com>
2024-05-01 15:31:16 +0200

    01.05.24

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -1,26 +1,32 @@
 #include "Fixed.hpp"
 
-//const int	Fixed::_frac = 8; //static variable of type const int and class Fixed, initialized to 8, _frac wont change during the program execution, shared by all instances of the class Fixed, numbers with fix comma are represented by 8 bits after the comma, this means values stored in the Fixed class will have 8 bits reserved for the decimal part 
+const int	Fixed::_frac = 8; //static variable of type const int and class Fixed, initialized to 8, _frac wont change during the program execution, shared by all instances of the class Fixed, numbers with fix comma are represented by 8 bits after the comma, this means values stored in the Fixed class will have 8 bits reserved for the decimal part 
 
-Fixed::Fixed(void) //: _value(0)
+Fixed::Fixed(void): _value(0)
 {
-	std::cout << "Fixed object created with default constructor" << std::endl; 
+	std::cout << "Default constructor called" << std::endl; 
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Fixed object destroyed" << std::endl; 
+	std::cout << "Destructor called" << std::endl; 
 }
 
 Fixed::Fixed(Fixed const & copy)  //copy constructor
 {
-	std::cout << "Fixed object copied" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
 	*this = copy; //assignation operator, copies the value of the object copy to the object creation, it make a true copy of the object copy
 }
 
-Fixed	&Fixed::operator=(const Fixed &copy)
+Fixed	&Fixed::operator=(const Fixed &copy)  //returns a reference to the object Fixed, 
 {
-	std::cout << "Assignment operator called" << std::endl;
+	std::cout << "Copy assignment operator called" << std::endl;
 	this->_value = copy.getRawBits();
 	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
 }
--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -6,16 +6,15 @@
 class Fixed
 {
     private:
-       // int					_value;
-	   // static const int	_frac;
+    	int					_value;
+	    static const int	_frac; //not useful yet
 
     public:
         Fixed(void); //default constructor canonical
 		~Fixed(void); //destructor canonical
 		Fixed(Fixed const &copy); //copy constructor canonical, creates a deep copy of the object passed as parameter, useful for dynamic memory allocation and agrregation by reference or by value
-		Fixed	&operator=(Fixed const &copy); //(operator=)assignation operator overload canonical, this operator is called when an already initialized object is assigned a new value from another object, it creates a deep copy of the object passed as parameter, useful for dynamic memory allocation and agrregation by reference or by value
-		//int		getRawBits(void) const;
-		//void	setRawBits(int const raw);
+		Fixed	&operator=(Fixed const &copy); //(operator=)assignation operator canonical, this operator is called when an already initialized object is assigned a new value from another object, it creates a deep copy of the object passed as parameter, useful for dynamic memory allocation and agrregation by reference or by value
+		int		getRawBits(void) const;
 };
 
 #endif
