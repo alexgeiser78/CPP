847115421863961e189cbc1ceca8bb905c2afabd
Alexandre Geiser <ageiser@car11s6.42barcelona.com>
2024-05-01 19:01:08 +0200

    01.05.24

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -17,9 +17,9 @@ static	float PfloattoInt(float base, int exp) //it converts a float number to an
 	return (result);
 }
 
-
 const int	Fixed::_frac = 8; //static variable of type const int and class Fixed, initialized to 8, _frac wont change during the program execution, shared by all instances of the class Fixed, numbers with fix comma are represented by 8 bits after the comma, this means values stored in the Fixed class will have 8 bits reserved for the decimal part 
 
+//--------------constructors----------------
 Fixed::Fixed(void): _value(0)
 {
 	std::cout << "Default constructor called" << std::endl; 
@@ -36,12 +36,17 @@ Fixed::Fixed(Fixed const & copy)
 	*this = copy;
 }
 
-Fixed	&Fixed::operator=(Fixed const &copy)
-{
-	std::cout << "Copy assignment operator called" << std::endl;
-	this->_value = copy.getRawBits();
-	return (*this);
+Fixed::Fixed(const int value): _value(value * PfloattoInt(2, this->_frac))
+{  
+	std::cout << "Int constructor called" << std::endl; 
+} 
+
+Fixed::Fixed(const float value): _value(value * PfloattoInt(2, this->_frac))
+{  
+	std::cout << "Float constructor called" << std::endl; 
 }
+
+//--------------functions----------------
 
 int	Fixed::getRawBits(void) const
 {
@@ -58,17 +63,45 @@ float	Fixed::toFloat(void) const   //fixed point number to float point number co
 	return (this->_value * PfloattoInt(2, -this->_frac));
 }
 
-Fixed::Fixed(const int value): _value(value * PfloattoInt(2, this->_frac))
-{  
-	std::cout << "Int constructor called" << std::endl; 
-} 
 
-Fixed::Fixed(const float value): _value(value * PfloattoInt(2, this->_frac))
-{  
-	std::cout << "Float constructor called" << std::endl; 
+
+//--------------operators----------------
+
+Fixed	&Fixed::operator=(Fixed const &copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_value = copy.getRawBits();
+	return (*this);
 }
+
+Fixed	Fixed::operator+(Fixed const &copy) const
+{
+	Fixed	result(this->toFloat() + copy.toFloat());
+	return (result);
+}
+
+Fixed	Fixed::operator-(Fixed const &copy) const
+{
+	Fixed	result(this->toFloat() - copy.toFloat());
+	return (result);
+}
+
+Fixed	Fixed::operator*(Fixed const &copy) const
+{
+	Fixed	result(this->toFloat() * copy.toFloat());
+	return (result);
+}
+
+Fixed	Fixed::operator/(Fixed const &copy) const
+{
+	Fixed	result(this->toFloat() / copy.toFloat());
+	return (result);
+}
+
+//--------------stream----------------
 
 std::ostream	&operator<<(std::ostream &str, Fixed const &fixed_nbr)  // definition of the operator <<, it takes a reference to an ostream object and a reference to a Fixed object, it returns a reference to an ostream object
 {
 	return (str << fixed_nbr.toFloat()); //it returns the float value of the Fixed object
 }
+
---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,31 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+    private:
+        int _value;
+        static const int _frac;
+    
+    public:
+        Fixed(void);
+        ~Fixed(void);
+        Fixed(Fixed const &copy);
+        Fixed(const int value);
+        Fixed(const float value);
+        
+        Fixed	&operator=(Fixed const &copy);
+        Fixed	operator+(Fixed const &copy) const;
+		Fixed	operator-(Fixed const &copy) const;
+		Fixed	operator*(Fixed const &copy) const;
+		Fixed	operator/(Fixed const &copy) const;
+        
+        int		getRawBits(void) const;
+        int		toInt(void) const;
+        float	toFloat(void) const;
+};
+        std::ostream	&operator<<(std::ostream &str, Fixed const &fixed_nbr);
+
+#endif
