f6fceb0c1795a87cfe0917c7b4531066bddda2dd
Alexandre Geiser <ageiser@car10s2.42barcelona.com>
2024-06-19 16:01:17 +0200

    19.06.24

---
+++ CPP04/ex00/Animal.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Pet.hpp"
+
+#pragma once
+
+class Animal
+{
+	protected:
+		std::string	_type;
+    
+    public:
+        Animal(void);
+		Animal(std::string const &type);
+		Animal(Animal const &copy);
+        virtual ~Animal(void); // virtual destructor ensure that the derived object is properly destroyed when called by a base pointer
+
+        Animal const	&operator=(Animal const &copy);
+
+        std::string const	&getType(void) const;
+		void				setType(std::string const &type);
+
+        virtual void		makeSound(void) const; //virtual means that it can be modified by derived classes, important for polymorphism
+};
---
+++ CPP04/ex00/Pet.cpp
@@ -0,0 +1,69 @@
+#include "Pet.hpp"
+
+Dog::Dog(void): _type("Dog")
+{
+    std::cout << "Dog from Animal " << Animal::_type << " created with default constructor." << std::endl;
+}
+
+Dog::Dog(std::string const &type): _type(type)
+{
+    std::cout << "Dog from Animal " << Animal::_type << " created." << std::endl;
+}
+
+Dog::Dog(Dog const &copy): Animal(copy)
+{
+    *this = copy;
+    std::cout << "Dog from Animal " << Animal::_type << " copied." << std::endl;
+}
+
+Dog::~Dog(void)
+{
+    std::cout << "Dog from Animal " << Animal::_type << " destroyed." << std::endl;
+}
+
+Dog const	&Dog::operator=(Dog const &copy)
+{
+    std::cout << "Assignment operator for Dog from Animal " << Animal::_type << " called." << std::endl;
+    Animal::operator=(copy);
+    return (*this);
+}
+
+void	Dog::makeSound(void) const
+{
+	std::cout << "Dog From Animal " << Animal::_type << " goes WafWaf!" << std::endl;
+}
+
+////////////////////////////////////////////////////////
+
+Cat::Cat(void): _type("Cat")
+{
+	std::cout << "Cat from Animal " << Animal::_type << " created with default constructor." << std::endl;
+}
+
+Cat::Cat(std::string const &type):Animal(type), _type("Cat")
+{
+	std::cout << "Cat from Animal " << Animal::_type << " created." << std::endl;
+}
+
+Cat::Cat(Cat const &copy): Animal(copy)
+{
+	*this = copy;
+	std::cout << "Cat from Animal " << Animal::_type << " copied." << std::endl;
+}
+
+Cat::~Cat(void)
+{
+	std::cout << "Cat from Animal " << Animal::_type << " destroyed." << std::endl;
+}
+
+Cat const	&Cat::operator=(Cat const &copy)
+{
+	std::cout << "Assignment operator for Cat from Animal " << Animal::_type << " called." << std::endl;
+	Animal::operator=(copy);
+	return (*this);
+}
+
+void	Cat::makeSound(void) const
+{
+	std::cout << "Cat From Animal " << Animal::_type << " goes Maow!" << std::endl;
+}
