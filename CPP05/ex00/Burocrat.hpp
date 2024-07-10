ca517bf96ff4bbf3d86a4fcb27e2d0ea0e238af6
alexgeiser78 <alexgeiser78@gmail.com>
2024-07-10 15:04:21 +0200

    10.07.24

---
+++ CPP05/ex00/Burocrat.hpp
@@ -0,0 +1,27 @@
+#ifndef BUROCRAT_HPP
+# define BUROCRAT_HPP
+
+# include <iostream>
+
+class Burocrat
+{
+    public:
+        Burocrat(void);
+        Burocrat(const Burocrat &obj);
+        ~Burocrat(void);
+        Burocrat &operator=(const Burocrat &obj);
+
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+# endif
---
+++ CPP05/ex00/main.cpp
@@ -0,0 +1,1 @@
+#include "Burocrat.hpp"
