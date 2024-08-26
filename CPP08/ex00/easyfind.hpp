10e40c6f24385c0332595983f63827ff9cc4433e
Alexandre Geiser <ageiser@car11s1.42barcelona.com>
2024-08-26 14:56:26 +0200

    26.08.24

---
+++ CPP08/ex00/easyfind.hpp
@@ -0,0 +1,34 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+//# include <algorithm>
+# include <exception>
+# include <iostream>
+
+class NotFoundException : public std::exception
+{
+public:
+    const char *what () const throw () //redifine the what method of the exception class, return a pointer to a const char
+    {
+        std::cout << what << std::endl; //
+        return "Element not found";
+    }
+};
+
+
+template <typename T> //template function able to work with any type of container, as long as it has iterators
+typename T::iterator easyfind(T &container, int n) //easyfind function returns an iterator "T::iterator". Typename is necessary to indicate that T::iterator is a type
+{
+    typename T::iterator iter; //declare an iterator of type T used to iterate through the container
+
+    iter = std::find(container.begin(), container.end(), n); //find method, search n in the container "container", returns an iterator pointing to the first element in the range [first, last). If no n is found, the function returns last.
+    if (iter == container.end()) //container.end != last element of the container, it is the element after the last element of the container  
+		throw NotFoundException();
+	return (iter);
+} 
+
+
+
+
+
+#endif
