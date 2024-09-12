d1c74dd99e916a78596f209735b8336275dcf21c
Alexandre Geiser <ageiser@car5s5.42barcelona.com>
2024-09-12 14:28:22 +0200

    21.09.24

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -2,6 +2,26 @@
 
 int main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+		std::cout << "Bad request" << std::endl;
+        return(1);
+    }
+
+        std::cout << "Before: ";
+        for (int i = 1; i < argc; i++)
+        {
+            std::cout << argv[i] << " ";
+        }
+        std::cout << std::endl;
+        
+        //std::vector<int> v1;
+
+        clock_t start = clock();
+
+        clock_t end = clock();
+
+        std::cout << "Time: " << static_cast<double>(end - start) / 1000 <<" millisec" << std::endl;
 
 
 
